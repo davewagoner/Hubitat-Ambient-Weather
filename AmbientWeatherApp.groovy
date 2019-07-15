addbcdc14dca3d132ae8ddfc8cd66dc061be4ba4
davewagoner <dave.wagoner@gmail.com>
2019-07-14 18:47:32 -0700

    Update AmbientWeatherApp.groovy

--- AmbientWeatherApp.groovy
+++ AmbientWeatherApp.groovy
@@ -1,54 +1,7 @@
-definition(name: "Ambient Weather API", namespace: "CordMaster", author: "Alden Howard", description: "A simple api for providing ambient weather access", iconUrl: "", iconX2Url: "");
+definition(name: "Weather.gov Temperature Forecast", namespace: "davewagoner", author: "David Wagoner", description: "Simple app to call weather.gov forecast API and return first day's forecast", iconUrl: "", iconX2Url: "");
 
 preferences {
-    page(name: "page1", title: "Log In", nextPage: "page2", uninstall: true) {
-        section {
-            input(name: "applicationKey", title: "Application Key", type: "text", required: true);
-            input(name: "apiKey", title: "API Key", type: "text", required: true);
-        }
-    }
-    
-    page(name: "page2");
-    page(name: "page3");
-}
 
-def page2() {
-    def stations = [];
-    def stationMacs = [];
-    try {
-        stations = getStations();
-        
-        stations.each { stationMacs << it.macAddress };
-    } catch(groovyx.net.http.HttpResponseException e) {
-        //then unauthorized
-        return dynamicPage(name: "page2", title: "Error", nextPage: "page1", uninstall: true) {
-            section {
-                paragraph("There was an error authorizing you. Please try again.");
-            }
-        }
-    }
-    
-   	log.debug("Got stations: " + stations);
-    
-	return dynamicPage(name: "page2", title: "Select Station", nextPage: "page3", uninstall: true) {
-		section {
-			input(name: "station", title: "Station", type: "enum", options: stationMacs, required: true);
-            input(name: "refreshInterval", title: "Refresh Interval (in minutes)", type: "number", range: "1..3600", defaultValue: 1, required: true);
-		}
-	}
-}
-
-def page3() {
-    dynamicPage(name: "page3", title: "Confirm Settings", install: true, uninstall: true) {
-        section {
-            paragraph("Selected station: $station");
-            paragraph("Refresh interval: $refreshInterval minute(s)");
-        }
-        
-        section {
-            paragraph("Press done to finish");
-        }
-    }
 }
 
 //lifecycle functions
@@ -88,17 +41,13 @@ def initialize() {
 }
 
 //children
-def addDevice() {
-	
-    addChildDevice("CordMaster", "Ambient Weather Device", "AWTILE-$station", null, [completedSetup: true]);
-}
 
 //fetch functions
 def getStations() throws groovyx.net.http.HttpResponseException {
     def data = [];
     
     def params = [
-        uri: "https://api.ambientweather.net/",
+        uri: "",
         path: "/v1/devices",
         query: [applicationKey: applicationKey, apiKey: apiKey]
     ];
@@ -122,9 +71,7 @@ def getWeather() throws groovyx.net.http.HttpResponseException {
 
 def requestData(path, query, code) {
     def params = [
-        uri: "https://api.ambientweather.net/",
-        path: path,
-        query: query
+        uri: "https://api.weather.gov/gridpoints/PQR/110,100/forecast",
     ];
     
     httpGet(params) { response ->
