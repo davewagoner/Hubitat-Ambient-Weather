6094b34263b39874915ae93fcc80c08a332e5390
davewagoner <dave.wagoner@gmail.com>
2019-07-17 07:45:00 -0700

    cleaned up

--- AmbientWeatherDevice.groovy
+++ AmbientWeatherDevice.groovy
@@ -16,40 +16,18 @@ metadata {
         }
     }
 }
+
 import groovy.json.JsonSlurper
 
-def getWeather() throws groovyx.net.http.HttpResponseException {
-    def data = [];
-    
-    requestData() { response ->
-        data = response.data;
-    };
-    log.debug("daveweather: " + data);
-	return data[0];
-}
-
-def requestData() {
-    def apiUrl = "https://api.weather.gov/gridpoints/PQR/110,100/forecast";
-    def card = new JsonSlurper().parse(apiUrl);
-    log.debug("reqdataslurper: " + card);
-    return card;
-}
-
-//loop
 def fetchNewWeather() {
     def apiUrl = "https://api.weather.gov/gridpoints/PQR/110,100/forecast";
     def card = new JsonSlurper().parse(apiUrl.toURL());
-    //def card = new JsonSlurper().parseText(apiUrl.text);
     def weatherMap = [:];
-    
-    def tempInt = card.properties.periods[0].temperature
+    def tempInt = card.properties.periods[0].temperature;
     weatherMap.temperature = Integer.toString(tempInt);
     weatherMap.icon = card.properties.periods[0].icon;
     //log.debug("reqdataslurper: " + Integer.toString(tempInt));
     setWeather(weatherMap);
-    return tempInt;
-	
-	
 }
 
 def refresh() {
@@ -63,11 +41,5 @@ def setWeather(weather){
 	//Set temperature
 	sendEvent(name: "temperature", value: weather.temperature, unit: '°F', isStateChange: true, displayed: true);
     sendEvent(name: "icon", value: weather.icon, isStateChange: true, displayed: true);
-}
-
-private logger(type, msg){
-	 if(type && msg && settings?.showLogs) {
-        log."${type}" "${msg}"
-    }
 }
 	
